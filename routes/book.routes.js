58c7542bb89f13dbe15077722eb7d2ebc0a5dba7
Paul Tiry <tirypaul@gmail.com>
2022-07-11 17:25:54 +0200

    finished CRUD

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -16,12 +16,59 @@ router.get('/books', (req, res, next) => {
     })
 })
 
+router.post('/books/delete', (req, res) => {
+  Book.findByIdAndDelete(req.body.bookId).then(() => {
+    res.redirect('/books')
+  })
+})
+
+router.get('/books/create', (req, res) => {
+  res.render('books/create-book')
+})
+
+router.post('/books/create', (req, res) => {
+  const newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    rating: req.body.rating,
+    description: req.body.description,
+  }
+
+  Book.create(newBook)
+    .then(() => {
+      res.redirect('/books')
+    })
+    .catch((err) => console.log(err))
+})
+
 router.get('/books/:bookId', (req, res, next) => {
   Book.findById(req.params.bookId)
     .then((book) => {
       res.render('books/book-details', book)
     })
     .catch((err) => console.log(err))
+})
+
+router.get('/books/:bookId/edit', (req, res) => {
+  Book.findById(req.params.bookId)
+    .then((book) => {
+      res.render('books/edit-book', book)
+    })
+    .catch((error) => {
+      console.log('Error getting book details from DB', error)
+      next(error)
+    })
+})
+
+router.post('/books/:bookId/edit', (req, res) => {
+  Book.findByIdAndUpdate(req.params.bookId, {
+    title: req.body.title,
+    author: req.body.author,
+    rating: req.body.rating,
+    description: req.body.description,
+  }).then(() => {
+    res.redirect('/books')
+  })
 })
 
 module.exports = router
