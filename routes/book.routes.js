37efff1bbb029c61407c9d9b183050f4f12c2333
Paul Tiry <tirypaul@gmail.com>
2022-07-11 15:07:54 +0200

    made book details page

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -6,12 +6,22 @@ router.get('/books', (req, res, next) => {
   Book.find()
     .then((books) => {
       const data = { booksArr: books }
-
+      if (req.query.minRating) {
+        data.booksArr = books.filter((book) => book.rating <= req.query.minRating)
+      }
       res.render('books/books-list', data)
     })
     .catch((error) => {
       console.log(error)
     })
+})
+
+router.get('/books/:bookId', (req, res, next) => {
+  Book.findById(req.params.bookId)
+    .then((book) => {
+      res.render('books/book-details', book)
+    })
+    .catch((err) => console.log(err))
 })
 
 module.exports = router
